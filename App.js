072e9d22eb6f837156fab4606cf94c11bd34c2f4
Bryan Kimani <brnkimani@gmail.com>
2022-06-13 15:28:45 +0300

    Replace ScrollView with FlatList component
    To enable lazy loading of list items

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 
 export default function App() {
 const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -10,7 +10,10 @@ const [courseGoals, setCourseGoals] = useState([]);
   };
 
   function addGoalHandler() {
-    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText]);
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals, 
+      {text: enteredGoalText, id: Math.random().toString()}
+    ]);
   };
   
   return (
@@ -20,13 +23,19 @@ const [courseGoals, setCourseGoals] = useState([]);
         <Button title='Add Goal' onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
-        <ScrollView>
-          { courseGoals.map((goal) => (
-            <View style={styles.goalItem} key={goal}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-            ))}
-        </ScrollView>
+        <FlatList 
+          data={courseGoals} 
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+           }}
+           keyExtractor={(item, index) => {
+             return  item.id;
+           }}
+         />
       </View>
     </View>
   );
